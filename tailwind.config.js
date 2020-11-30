ec7ba86146be4e74a85d3be90aa452c16d8de863
DreamingInBinary <info@dreaminginbinary.co>
2020-11-30 10:05:55 -0600

    Who even know what this is from last week but Im commiting it anyways

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-const defaultTheme = require("node_moduels/tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const plugin = require("tailwindcss/plugin");
+
 // Figure this out
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', 
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  variants: {},
+  plugins: [require("@tailwindcss/typography")],
 };
