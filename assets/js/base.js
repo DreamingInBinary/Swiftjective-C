4c302d9f6dc1a20bb3e689b2c5e4e0946b1aa975
DreamingInBinary <info@dreaminginbinary.co>
2020-12-17 17:51:39 -0600

    Google search

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,11 +1,29 @@
 function fixTwitter() {
-    var tweets = document.getElementsByClassName('jekyll-twitter-plugin');
+    let tweets = document.getElementsByClassName('jekyll-twitter-plugin');
     
     for (var i=0, len=tweets.length|0; i<len; i=i+1|0) {
         tweets[i].classList.add('mx-auto', 'max-w-md');
     }
 }
 
+function performSiteSearch() {
+    alert("Suhhhhh");
+}
+
 window.onload = function(){ 
     fixTwitter();
+
+    // Find search bar
+    let search = document.getElementById("search");
+    search.addEventListener('keyup', ({key}) => {
+        if (key === "Enter") {
+            var advGoogleSearch = "site:\"swiftjectivec.com\"";
+            advGoogleSearch += " ";
+            advGoogleSearch += "\"";
+            advGoogleSearch += search.value;
+            advGoogleSearch += "\"";
+            let searchQuery = "https://www.google.com/search?q=" +  advGoogleSearch;
+            window.open(searchQuery, '_blank');
+        }
+    })
 }
